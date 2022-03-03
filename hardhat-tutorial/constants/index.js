44508ebe91354796894c1c39a249ea400012752d
AleMantov <malthar@gmail.com>
2022-03-03 17:37:31 +0100

    ready to deploy on rinkeby

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x07DDB26ee523e681940069cA1DFD9a50286C77F1";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
